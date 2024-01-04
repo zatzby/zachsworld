0c33ca9eccd5596063cb00b879c4adb4122d0054
zach <128881293+zatzby@users.noreply.github.com>
2024-01-04 09:52:47 -0800

    Update books.ts

--- redeux/src/lib/data/books.ts
+++ redeux/src/lib/data/books.ts
@@ -1,12 +1,24 @@
 export const bookLinks: App.LinkList[] = [
+    // write my own descriptions of all books with personal notes
     {
-        title: 'Books I Like',
+        title: 'Books I Like (in no particular order)',
         links: [
             {
                 title: 'The Price of Tomorrow by Jeff Booth',
                 url: 'https://thepriceoftomorrow.com/',
                 description: 'Technological advances are happening at a rate faster than our ability to understand them, and in a world that moves faster than we can imagine, we cannot afford to stand still.'
+            },
+            {
+                title: "Childhood's End by Arthur C. Clarke",
+                url: 'https://www.amazon.com/Childhoods-End-Arthur-C-Clarke/dp/110196703X',
+                description: 'Spaceships have suddenly appeared in the skies above every city on the planet. Inside is an intellectually, technologically, and militarily superior alien race known as the Overlords. At first, their demands seem benevolent: unify Earth, eliminate poverty, end war. But at what cost?'
+            },
+            {
+                title: 'The Will to Meaning by Viktor Frankl',
+                url: 'https://www.amazon.com/Will-Meaning-Foundations-Applications-Logotherapy/dp/0142181269',
+                description: "Originally published in 1969 and compiling Frankl's speeches on logotherapy, The Will to Meaning is regarded as a seminal work of meaning-centered therapy."
             }
         ]
     }
 ];
+
