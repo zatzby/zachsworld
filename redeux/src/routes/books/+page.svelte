cf0c0829ee8dc23c9a89bb4c08aaae8764c296f7
zach <128881293+zatzby@users.noreply.github.com>
2024-01-04 09:52:26 -0800

    Update +page.svelte

--- redeux/src/routes/books/+page.svelte
+++ redeux/src/routes/books/+page.svelte
@@ -2,13 +2,13 @@
     import { bookLinks } from "$lib/data/books";
 </script>
 
-<h1>Read books.</h1>
+<h1 class="mt-20 mb-20 text-4xl text-center">Read books. Learn stuff. Move forward.</h1>
 
 {#each bookLinks as linkList}
-    <h2>{linkList.title}</h2>
-    <ul>
+    <h2 class="text-3xl ml-20">{linkList.title}</h2>
+    <ul class="text-2xl mt-10">
         {#each linkList.links as link}
-        <li>
+        <li class="mb-10 ml-20">
             <a href={link.url} target="_blank">{link.title}</a> &ndash; {link.description}
         </li>
         {/each}
