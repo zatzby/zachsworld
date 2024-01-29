5d7c8be2f70e93f5d994cc69ad41cc3761fd77df
zach <zach@fastlnnode.com>
2024-01-29 15:31:11 -0800

    update gpg + added books

--- redeux/src/lib/data/books.ts
+++ redeux/src/lib/data/books.ts
@@ -6,17 +6,62 @@ export const bookLinks: App.LinkList[] = [
             {
                 title: 'The Price of Tomorrow by Jeff Booth',
                 url: 'https://thepriceoftomorrow.com/',
-                description: 'Technological advances are happening at a rate faster than our ability to understand them, and in a world that moves faster than we can imagine, we cannot afford to stand still.'
+                description: ''
             },
             {
                 title: "Childhood's End by Arthur C. Clarke",
                 url: 'https://www.amazon.com/Childhoods-End-Arthur-C-Clarke/dp/110196703X',
-                description: 'Spaceships have suddenly appeared in the skies above every city on the planet. Inside is an intellectually, technologically, and militarily superior alien race known as the Overlords. At first, their demands seem benevolent: unify Earth, eliminate poverty, end war. But at what cost?'
+                description: ''
             },
             {
                 title: 'The Will to Meaning by Viktor Frankl',
                 url: 'https://www.amazon.com/Will-Meaning-Foundations-Applications-Logotherapy/dp/0142181269',
-                description: "Originally published in 1969 and compiling Frankl's speeches on logotherapy, The Will to Meaning is regarded as a seminal work of meaning-centered therapy."
+                description: ''
+            },
+            {
+                title: 'The Goal by Eliyahu Goldratt',
+                url: 'https://www.amazon.com/Goal-Process-Ongoing-Improvement/dp/0884271951',
+                description: ''
+            },
+            {
+                title: 'Lie Down in Darkness by William Styron',
+                url: 'https://www.amazon.com/Lie-Down-Darkness-William-Styron/dp/0679735976',
+                description: ''
+            },
+            {
+                title: 'Siddharta by Hermann Hesse',
+                url: 'https://www.amazon.com/Siddhartha-Novel-Hermann-Hesse/dp/0553208845',
+                description: ''
+            },
+            {
+                title: 'Antifragile by Nassim Nicholas Taleb',
+                url: 'https://www.amazon.com/Antifragile-Things-That-Disorder-Incerto/dp/0812979680',
+                description: ''
+            },
+            {
+                title: 'When Money Dies by Adam Fergusson',
+                url: 'https://www.amazon.com/When-Money-Dies-Devaluation-Hyperinflation/dp/1586489941',
+                description: ''
+            },
+            {
+                title: 'The Bitcoin Standard by Saifedean Ammous',
+                url: 'https://www.amazon.com/Bitcoin-Standard-Decentralized-Alternative-Central/dp/1119473861',
+                description: ''
+            },
+            {
+                title: 'The Mandibles by Lionel Shriver',
+                url: 'https://www.amazon.com/Mandibles-Family-2029-2047-Lionel-Shriver/dp/006232828X',
+                description: ''
+            },
+            {
+                title: 'Start With No by Jim Camp',
+                url: 'https://www.amazon.com/Start-Negotiating-Tools-that-Pros/dp/0609608002',
+                description: ''
+            },
+            {
+                title: 'Snow Crash by Neal Stephenson',
+                url: 'https://www.amazon.com/Snow-Crash-Neal-Stephenson/dp/0553380958',
+                description: '',
             }
         ]
     }
--- redeux/src/routes/books/+page.svelte
+++ redeux/src/routes/books/+page.svelte
@@ -2,15 +2,16 @@
     import { bookLinks } from "$lib/data/books";
 </script>
 
-<h1 class="mt-20 mb-20 text-4xl text-center">Read books. Learn stuff. Move forward.</h1>
+<h1 class="mt-20 mb-20 pb-10 text-4xl text-center text-orange-600">Read books. Learn stuff. Move forward.</h1>
 
 {#each bookLinks as linkList}
-    <h2 class="text-3xl mx-20">{linkList.title}</h2>
+    <h2 class="text-center text-3xl mx-20 pb-5">{linkList.title}</h2>
     <ul class="text-2xl mt-10">
         {#each linkList.links as link}
-        <li class="mb-10 mx-20">
-            <a href={link.url} target="_blank">{link.title}</a> &ndash; {link.description}
+        <li class="mb-10 mx-20 ">
+            <a href={link.url} target="_blank">{link.title}</a>
         </li>
         {/each}
     </ul>
 {/each}
+<br />
